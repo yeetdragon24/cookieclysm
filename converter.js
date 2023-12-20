a084232a77d79aba55f78385c16d734a2c340058
FizzWizZleDazzle <126825095+FizzWizZleDazzle@users.noreply.github.com>
2023-12-19 19:10:27 -0500

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,5 +1,20 @@
-Game.LoadMod("https://unpkg.com/cppkies")
+Game.LoadMod("https://raw.githubusercontent.com/yeetdragon24/cookieclysm/main/OtherGameLoader.js")
 if (!window.CPPKIES_ONLOAD) CPPKIES_ONLOAD = []
-CPPKIES_ONLOAD.push(() => {
-    new Cppkies.Upgrade("Hello, World!", "My first upgrade!", 7, [10, 5])
+Cppkies.onLoad.push(() => {
+    Cppkies.buildingLink = `https://github.com/yeetdragon24/cookieclysm/blob/main/img/building.png`
+    Cppkies.iconLink = `https://github.com/yeetdragon24/cookieclysm/blob/main/img/building.png`
+    new Cppkies.Building(
+        "Cppkie Baker",
+        "cppkie baker|cppkie bakers|baked|[X] bug fix|[X] bug fixes",
+        "Generates cookies by baking them, why did nobody think about this?",
+        [0, 0],
+        {
+            bg: `https://github.com/yeetdragon24/cookieclysm/blob/main/img/building.png`,
+            pic: `https://github.com/yeetdragon24/cookieclysm/blob/main/img/building.png,
+            yV: 64,
+            xV: 16,
+        },
+        Cppkies.DEFAULT_CPS, // Your buildings cps
+        Cppkies.DEFAULT_ONBUY // The function to call when your building gets bought but don't worry about this
+    )
 })
