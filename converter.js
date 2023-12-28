a84d2ac0359a7882349af02c306b6f2e1f5780c5
Mentally Distressed Programmer <126825095+FizzWizZleDazzle@users.noreply.github.com>
2023-12-28 18:24:32 -0500

    Finishing touches

--- converter.js
+++ converter.js
@@ -1,12 +1,12 @@
 import Cppkies from "cppkies"
-const prefix = "https://example.com/CppkieBaker/static"
+const prefix = "https://github.com/yeetdragon24/cookieclysm/tree/main/img"
 Cppkies.onLoad.push(() => {
     Cppkies.buildingLink = `${prefix}/buildingBigIcon.png`
     Cppkies.iconLink = `${prefix}/buildingIcons.png`
     new Cppkies.Building(
-        "Cppkie Baker", // The Name of your building
-        "cppkie baker|cppkie bakers|baked|[X] bug fix|[X] bug fixes", // Name of your building in a sentence, and then it plural, then what boosts your building when a sugar lump is added, then it plural
-        "Generates cookies by baking them, why did nobody think about this?", // How your building generates cookies
+        "Converter", // The Name of your building
+        "Converter|Converters|Convertered|[X] more dense|[X] more dense", // Name of your building in a sentence, and then it plural, then what boosts your building when a sugar lump is added, then it plural
+        "Generates cookies by converting living matter into cookies", // How your building generates cookies
         [0, 0], // The coordinates for the small, tooltip icon for your building
         [0, 0], // The coordinates for the big icon in the store for your building
         {
@@ -18,7 +18,7 @@ Cppkies.onLoad.push(() => {
         Cppkies.DEFAULT_CPS, // Your building's cps
         Cppkies.DEFAULT_ONBUY, // The function to call when your building gets bought but don't worry about this
         {
-            name: "Normal Baker",
+            name: "Converter",
             desc: "The top of your cookie hierarchy",
             icon: [2, 0],
         }, // Your building's data on business day
