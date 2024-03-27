9a1fec21d832f7e39e59677e9ee3a6a5161dc8b2
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-03-27 17:00:16 -0400

    fixed visual bugs + upgrade names

--- converter.js
+++ converter.js
@@ -10,9 +10,10 @@ new Game.Object('Converter','converter|converters|converted|[X] extra chamber|[X
 		});
 */
 //ripped straight from ccse because i dont want to use ccse
-NewBuilding = function(name, commonName, desc, icon, iconColumn, art, price, cps, buyFunction, foolObject, buildingSpecial){
+
+var NewBuilding = function(name, commonName, desc, icon, iconColumn, art, price, cps, buyFunction, foolObject, buildingSpecial){
 		var me = new Game.Object(name, commonName, desc, icon, iconColumn, art, price, cps, buyFunction);
-		
+		console.log(me.canvas)
 		// This is the name, description, and icon used during Business Season
 		if(foolObject) Game.foolObjects[name] = foolObject;
 		// The name of this building's golden cookie buff and debuff
@@ -84,7 +85,7 @@ NewBuilding = function(name, commonName, desc, icon, iconColumn, art, price, cps
 		return me;
 	}
 NewBuilding('Converter','converter|converters|converted|[X] extra chamber|[X] extra chambers','Converts living mass into cookies.',21,12,
-{pic:'https://raw.githubusercontent.com/yeetdragon24/cookieclysm/main/img/converterIcon.png',bg:'img/factoryBackground.png',xV:8,yV:8,w:64,rows:2,x:0,y:16},"this is two years old",
+{pic:'https://raw.githubusercontent.com/yeetdragon24/cookieclysm/main/img/building.png',bg:'img/factoryBackground.png',xV:8,yV:8,w:64,rows:1,x:0,y:16},"this is two years old",
 			function(me){
 				var mult = 1;
 				mult *= Game.GetTieredCpsMult(me);
@@ -101,8 +102,48 @@ NewBuilding('Converter','converter|converters|converted|[X] extra chamber|[X] ex
 				icon:1
 			},
 			['Kugelblitz', 'Spaghettification']);
+//reconnected buildings to their canvases (for some reason adding a new one does that)
+for (let i in Game.Objects) {
+    if (i!='Cursor') {
+		Game.Objects[i].canvas=l('rowCanvas'+Game.Objects[i].id);
+		Game.Objects[i].ctx=l('rowCanvas'+Game.Objects[i].id).getContext('2d');
+	}
+}
 var converter=Game.Objects['Converter'];
-upAndAchiev.push(Game.GrandmaSynergy('Massive grandmas','A large grandma to be converted into more cookies.','Converter'));Game.last.order=256;
+Game.GrandmaSynergy('Massive grandmas','A large grandma to be converted into more cookies.','Converter');Game.last.order=256;
+Game.TieredUpgrade('Alien volunteers','<q>They don\'t know what they are volunteering for.</q>','Converter',1);
+Game.TieredUpgrade('Salty electrons','<q>Electrons are now required to be converted as well instead of retaining their structure, and as a result are salty.</q>','Converter',2);
+Game.TieredUpgrade('Box hypothesis','<q>(can’t think of one, make new)</q> <span style="right:0;position:absolute;">-Stream Sniper</span>','Converter',3);
+Game.TieredUpgrade('Small dough converters','<q>Some... "experiments" have left you with live cookie dough, and you see this as an opportunity.</q>','Converter',4);
+Game.TieredUpgrade('Oven explosion','<q>And that\'s how it all ended, for the operator too.</q>','Converter',5);
+Game.TieredUpgrade('Cyclotron','<q>These can make particles spin, and make them collide too. Now how can you use this to make more cookies?</q>','Converter',6);
+Game.TieredUpgrade('Macrocosmics','<q>According to the theory of nanocosmics, our universe is just a subatomic particle in another universe, maybe even a subatomic particle in a living organism.</q>','Converter',7);
+//Game.TieredUpgrade('The Beat','<q>If the matter and antimatter both fall out of The Pulse at any time, you somehow get cookies.</q>','Converter',8);
+Game.TieredUpgrade('Probably dust motes','<q>A very large nonessential particle.</q>','Converter',8);
+Game.TieredUpgrade('Quantum knotting','<q>Re-entangle quantum particles so more gets converted with less.</q>','Converter',9);
+Game.TieredUpgrade('Ig Nobel Prize','<q>Why reward only your actions and make people skeptical, when you can reward all mundane events that happen?</q>this is a real thing I promise','Converter',10);
+Game.TieredUpgrade('sqrt(-1)','<q>When there\'s nothing left to convert, convert imaginary things</q>','Converter',11);
+Game.TieredUpgrade('Hunger','<q>You\'ve discovered hunger in a tangible form. Put it in your cookies. People who eat them will get hungrier, and need to eat more cookies.</q>','Converter',12);
+Game.TieredUpgrade('Cookie fusion','<q>Atoms can now push other atoms to force them to merge, and this somehow creates cookies.</q>','Converter',13);
+Game.TieredUpgrade('Consumer enlargement','<q>Not only do they have to eat more, they also can be converted into more.</q>','Converter',14);
+Game.TieredUpgrade('Unsweetened quarks','<q>Filler for your cookies. It doesn\'t taste like anything, but they won\'t notice.</q>','Converter',15);
+
+Game.TieredAchievement('','','Converter',1);
+Game.TieredAchievement('','','Converter',2);
+Game.TieredAchievement('','','Converter',3);
+Game.TieredAchievement('','','Converter',4);
+Game.TieredAchievement('','','Converter',5);
+Game.TieredAchievement('','','Converter',6);
+Game.TieredAchievement('','','Converter',7);
+Game.TieredAchievement('','','Converter',8);
+Game.TieredAchievement('','','Converter',9);
+Game.TieredAchievement('','','Converter',10);
+Game.TieredAchievement('','','Converter',11);
+Game.TieredAchievement('','','Converter',12);
+Game.TieredAchievement('','','Converter',13);
+Game.TieredAchievement('','','Converter',14);
+Game.TieredAchievement('','','Converter',15);
+LocalizeUpgradesAndAchievs();
 converter.desc='Converts living matter into cookies.'; //right now mod is only english so this avoids loc issues
 converter.grandma=Game.Upgrades['Massive grandmas'];
 converter.l.childNodes[0].style.background=`url('https://raw.githubusercontent.com/yeetdragon24/cookieclysm/main/img/converterIcon.png')`;
