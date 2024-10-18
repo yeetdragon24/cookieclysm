a6edca1dbca1681b7535186d475e7c15b1b7a8cc
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-10-18 18:33:01 -0400

    somewhat broken

--- cookieclysm.loader.js
+++ cookieclysm.loader.js
@@ -1,29 +1,48 @@
 Game.registerMod('Cookieclysm', {
 	init: function() {
-		this.SwitchSave=function(){
-			Game.WriteSave();
-			Game.SaveTo='cclysmGame';
+		if (localStorage.getItem('cclysmGame') === null) {
+			this.startingPrompt();
 		}
-		this.WipeCookieclysm=function(){
-			delete localStorage['cclysmGame'];
+		else {
+			this.switchSave();
+			this.loadScripts();
 		}
-
-		this.loadScripts();
 	},
 	load: function(str) {
-		window.CookieclysmLoadStr = str;
+		Game.mods['Cookieclysm'].loadStr = str;
 	},
 	save: function() {
-		if (C.save) return C.save();
+		if (typeof C !== 'undefined' && C.save) return C.save();
 		else return '';
 	},
 	loadScripts: function() {
-		let dir='https://yeetdragon24.github.io/cookieclysm/';
-		LoadScript(dir + 'cookieclysm.js', function() {
-		LoadScript(dir + 'transcensions.js', function() {
-		LoadScript(dir + 'youMinigame.js', function() {
-		LoadScript(dir + 'sniperGold.js', function() {
-		LoadScript(dir + 'wizardPortal.js')
+		const Mod = this;
+		const path = 'https://yeetdragon24.github.io/cookieclysm/';
+		LoadScript(path + 'cookieclysm.js', function() {
+		LoadScript(path + 'transcensions.js', function() {
+        LoadScript(path + 'youMinigame.js', function() {
+        LoadScript(path + 'sniperGold.js', function() {
+		LoadScript(path + 'wizardPortal.js', function() { C.toLoad = true; Mod.load(Game.modSaveData['Cookieclysm']); })
 		})})})});
+	}, 	
+	switchSave: function() {
+		Game.WriteSave();
+		Game.SaveTo = 'cclysmGame';
+		Game.LoadSave();
+	},
+	wipeCookieclysm: function(){
+		localStorage.removeItem('cclysmGame');
+	},
+	startingPrompt: function() {
+		Game.Prompt(`<id CheckCclysm>
+			<h3>Cookieclysm</h3>
+			<div class="block">
+				Some features of Cookieclysm require the game to use different save data than vanilla Cookie Clicker. The vanilla save data is not modified while Cookieclysm is loaded, and loadingCookieclysm will automatically load the Cookieclysm save data.
+				<div class="line"></div>
+				Cookieclysm creates save data using the existing save data, so you'll start off where you left off in vanilla Cookie Clicker.
+				<div class="line"></div>
+				Clicking "Got it" will save the game, create the new save data, and start using the new save data.
+			</div>`,
+		[['Got it','Game.mods[\'Cookieclysm\'].switchSave();Game.mods[\'Cookieclysm\'].loadScripts();Game.ClosePrompt();'], ['No','delete Game.mods[\'Cookieclysm\']; console.log(\'Mod "Cookieclysm" removed.\'); Game.ClosePrompt();']]);
 	}
 });
