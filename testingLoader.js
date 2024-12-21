d7fc1c6413e65b0b79e672cd351168d71ce5e554
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-12-21 16:05:25 -0500

    Create testingLoader.js

---
+++ testingLoader.js
@@ -0,0 +1,56 @@
+Game.registerMod('Cookieclysm', {
+	init: function() {
+        window.Cookieclysm = this;
+		if (localStorage.getItem('cclysmGame') === null) {
+			this.startingPrompt();
+		}
+		else {
+			this.switchSave();
+			this.loadScripts();
+		}
+	},
+	load: function(str) {
+        if (!Cookieclysm.loadStr) Cookieclysm.loadStr = str;
+	},
+	save: function() {
+		if (typeof C !== 'undefined' && C.save) return C.save();
+		else return '';
+	},
+	loadScripts: function() {
+		const Mod = this;
+		const path = 'https://raw.githack.com/yeetdragon24/cookieclysm/testing/';
+		LoadScript(path + 'cookieclysm.js', function() {
+		LoadScript(path + 'transcensions.js', function() {
+        LoadScript(path + 'youMinigame.js', function() {
+        LoadScript(path + 'sniperGold.js', function() {
+		LoadScript(path + 'wizardPortal.js', function() { Game.mods['Cookieclysm'].loaded = true; Mod.toLoad = true; })
+		})})})});
+	}, 	
+	switchSave: function() {
+		Game.WriteSave();
+		Game.SaveTo = 'cclysmGame';
+		Game.LoadSave();
+	},
+	wipeCookieclysm: function(){
+		localStorage.removeItem('cclysmGame');
+	},
+    unloadCookieclysm: function() {
+        delete Game.mods['Cookieclysm'];
+        delete window.Cookieclysm;
+        delete window.C;
+    },
+	startingPrompt: function() {
+		Game.Prompt(`<id CheckCclysm>
+			<h3>Cookieclysm</h3>
+			<div class="block">
+				Some features of Cookieclysm require the game to use different save data than vanilla Cookie Clicker. The vanilla save data is not modified while Cookieclysm is loaded, and loadingCookieclysm will automatically load the Cookieclysm save data.
+				<div class="line"></div>
+				Cookieclysm creates save data using the existing save data, so you'll start off where you left off in vanilla Cookie Clicker.
+				<div class="line"></div>
+				Clicking "Got it" will save the game, create the new save data, and start using the new save data.
+                <div class="line"></div>
+                Join our <a href="https://discord.gg/bR4qKSJuCS">Discord server</a>!
+			</div>`,
+		[['Got it','Cookieclysm.switchSave(); Cookieclysm.loadScripts();Game.ClosePrompt();'], ['No','Cookieclysm.unloadCookieclysm(); console.log(\'Mod "Cookieclysm" removed.\'); Game.ClosePrompt();']]);
+	}
+});
